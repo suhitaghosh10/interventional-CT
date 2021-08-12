07fe9a1cf1cbc27e24f3bcd91ceb17948eb1e2d8
suhita <suhitaghosh10@users.noreply.github.com>
2021-08-12 21:45:03 +0200

    Add files via upload
    initial commit

---
+++ utility/logger_utils_prior.py
@@ -0,0 +1,183 @@
+from utility.common_imports import *
+from matplotlib import pyplot as plt
+from utility.constants import *
+from utility.utils import idct_blockwise, augment_prior
+from dataset.head_carm.constants import CARM_DCT_MIN, CARM_DCT_MAX
+import io
+
+
+class PlotReconstructionCallback(tfk.callbacks.Callback):
+    """Plot reconstructed images to tensorboard."""
+
+    def __init__(self, logdir: str, test_ds: tf.data.Dataset,
+                 batch_size=4,
+                 model_type=None,
+                 save_by='val_loss',
+                 save_by_decrease=True,
+                 batched=False,
+                 is_shuffle=False,
+                 shuffle_buffer_size=128,
+                 is_augment=True,
+                 log_on_epoch_end=True,
+                 step_num=100,
+                 chkpoint_path=None,
+                 init_epoch=1):
+
+        super(PlotReconstructionCallback, self).__init__()
+        self.model_type = model_type
+        self.logdir = logdir
+        self.save_by_key = save_by
+        self.save_by_decrease = save_by_decrease
+        self.batch_size = batch_size
+        self.epoch_num = init_epoch + 1
+        self.chkpoint_path = chkpoint_path
+        self.log_on_epoch_end = log_on_epoch_end
+        self.save_by_val = tf.constant(np.inf, dtype=tf.float64) if self.save_by_decrease else tf.constant(-np.inf,
+                                                                                                           dtype=tf.float64)
+        if not log_on_epoch_end:
+            self.steps = step_num
+
+        _logdir_recons = os.path.join(logdir, 'reconstructions')
+        self.file_writer = tf.summary.create_file_writer(logdir=_logdir_recons)
+        # dataset stuff
+        if is_shuffle:
+            test_ds = test_ds.shuffle(shuffle_buffer_size, seed=123)
+        if is_augment:
+            test_ds = test_ds.map(augment_prior)
+        if batched:
+            self.test_ds = test_ds.unbatch()
+        self.test_ds = test_ds.batch(batch_size)
+        self.test_it_ds = iter(self.test_ds)
+
+    def on_epoch_end(self, epoch, logs=None):
+        if tf.equal(epoch, 0):
+            self.model.summary()
+        if self.log_on_epoch_end:
+            self._log_images(epoch)
+
+        self._save_wts(logs)
+        self.epoch_num += 1
+
+    def on_batch_end(self, batch, logs=None):
+        if (not self.log_on_epoch_end) and (batch % self.steps == 0):
+            self._log_images((batch + 1) * (self.epoch_num))
+            print('\nLogging for step no {} and epoch {}.'.format(batch + 1, self.epoch_num))
+
+    def _get_next_images(self):
+        try:
+            next_images_x, next_images_y = next(self.test_it_ds)
+        except StopIteration:
+            self.test_it_ds = iter(self.test_ds)
+            next_images_x, next_images_y = next(self.test_it_ds)
+        return next_images_x, next_images_y
+
+    def _plot_img_reconstruction(self, images, gt, reconstruction):
+        fig, ax = plt.subplots(nrows=2, ncols=2)
+        fig.set_size_inches(30, 30)
+        image = images[0]
+        p_image = images[1]
+        if self.model_type is MODEL_TYPE_DCT:
+            dct_diff = (CARM_DCT_MAX - CARM_DCT_MIN)
+            image = (image * dct_diff) + CARM_DCT_MIN
+            gt = (gt * dct_diff) + CARM_DCT_MIN
+            reconstruction = (reconstruction * dct_diff) + CARM_DCT_MIN
+
+            image = idct_blockwise(tf.cast(image, tf.float32))
+            gt = idct_blockwise(tf.cast(gt, tf.float32))
+            reconstruction = idct_blockwise(tf.cast(reconstruction, tf.float32))
+            # reconstruction = (reconstruction - tf.reduce_min(reconstruction)) \
+            #                  / (tf.reduce_max(reconstruction) - tf.reduce_min(reconstruction))
+
+        if image.shape[-1] == 1:
+            image = tf.cast(tf.squeeze(image, axis=-1), dtype=tf.float32)
+            reconstruction = tf.cast(tf.squeeze(reconstruction, axis=-1), dtype=tf.float32)
+            gt = tf.cast(tf.squeeze(gt, axis=-1), dtype=tf.float32)
+
+        # if len(img_shape)> 3: # 3D data
+        #     ax[0, 0].imshow(image[:,:,img_shape[2]//2], vmin=0., vmax=1., cmap=plt.cm.gray)
+        # elif img_shape[-1] == 1:
+        #     ax[0,0].imshow(image, cmap=plt.cm.gray)
+        # else:
+        #     ax[0,0].imshow(image, vmin=0., vmax=1.)
+        diff = tf.abs(gt - reconstruction)
+
+        # txt = str(np.min(image))+str(np.max(image))
+        # ax[0,0].set_title('Image'+txt)
+        # mset = mse(image, gt).numpy()
+        # ssimt = ssim(image, gt).numpy()
+        # psnrt = psnr(image, gt).numpy()
+        # text = str(mset)+','+str(ssimt)+','+str(psnrt)
+        # ax[0,0].set_title(text)
+        ax[0, 0].imshow(image, cmap=plt.cm.gray, vmin=0., vmax=1.)
+        ax[0, 0].axis('off')
+        ax[0, 1].imshow(reconstruction, cmap=plt.cm.gray, vmin=0., vmax=1.)
+        # ax[0, 1].xlabel (text, fontsize=24)
+        ax[0, 1].axis('off')
+        ax[1, 0].imshow(gt, cmap=plt.cm.gray, vmin=0., vmax=1.)
+        ax[1, 0].axis('off')
+        ax[1, 1].imshow(p_image, vmin=0., vmax=1., cmap=plt.cm.gray)
+        ax[1, 1].axis('off')
+
+        return fig
+
+    def _log_images(self, step_num):
+        if self.batch_size < 4:
+            batch_num = self.batch_size // 4
+        else:
+            batch_num = 1
+
+        for b in range(batch_num):
+            if b == 0:
+                images, gts = self._get_next_images()
+                reconstructions = self.model(images)
+            else:
+                temp_img, temp_gt = self._get_next_images()
+                temp_recons = self.model(temp_img)
+                images = tf.concat([images, temp_img], axis=0)
+                gts = tf.concat([gts, temp_gt], axis=0)
+                reconstructions = tf.concat([reconstructions, temp_recons], axis=0)
+
+        imgs = []
+        try:
+            for i in range(self.batch_size):
+                fig = self._plot_img_reconstruction(images[i], gts[i], reconstructions[i])
+                imgs.append(_plot_to_image(fig))
+            imgs = tf.concat(imgs, axis=0)
+            with self.file_writer.as_default():
+                tf.summary.image(
+                    name='Reconstructions',
+                    data=imgs,
+                    step=step_num,
+                    max_outputs=self.batch_size
+                )
+                # tf.summary.scalar('lr', self.model.optimizer.lr, step=step_num)
+        except:
+            traceback.print_exc()
+
+    def _save_wts(self, logs):
+
+        save_by_condn = self.save_by_val > logs[self.save_by_key] if self.save_by_decrease else self.save_by_val < logs[
+            self.save_by_key]
+        indc = 'decreased' if self.save_by_decrease else 'increased'
+        if self.save_by_key in logs and save_by_condn:
+            print('\n', self.save_by_key, indc, 'from', self.save_by_val, 'to', logs[self.save_by_key])
+            self.save_by_val = logs[self.save_by_key]
+            self.model.save_weights(self.chkpoint_path + '.hdf5')
+            print("Saved checkpoint for epoch {}: {}".format(str(self.epoch_num), self.chkpoint_path))
+
+
+def _plot_to_image(figure):
+    """Converts the matplotlib plot specified by 'figure' to a PNG image and
+    returns it. The supplied figure is closed and inaccessible after this call."""
+    # Save the plot to a PNG in memory.
+    buf = io.BytesIO()
+    plt.savefig(buf, format='png')
+    # Closing the figure prevents it from being displayed directly inside
+    # the notebook.
+    plt.close(figure)
+    buf.seek(0)
+    # Convert PNG buffer to TF image
+    image = tf.image.decode_png(buf.getvalue(), channels=4)
+    # Add the batch dimension
+    image = tf.expand_dims(image, 0)
+    return image
---
+++ utility/utils.py
@@ -0,0 +1,201 @@
+from utility.common_imports import *
+from utility.constants import *
+import tensorflow_addons as tfa
+#from utility.tensorflow_imported.filter_ops import gaussian, laplacian
+AUGMENTATION_MAX_ANGLE = 1.0 #radian
+GAUSSIAN_SIGMA = 1.
+GAUSSIAN_LAPLACIAN_K_SIZE=7
+
+def ssim(dim, paddings=None):
+    def masked_ssim(y_true, y_pred):
+        if paddings is not None:
+            temp = tf.constant(value=1.0, shape=dim)
+            mask = tf.pad(temp, paddings=paddings)
+            y_true = mask * y_true
+            y_pred = mask * y_pred
+        mssim = tf.image.ssim(y_true, y_pred, max_val=1.)
+        return mssim
+
+    return masked_ssim
+
+def psnr(dim, paddings=None):
+        def masked_psnr(y_true, y_pred):
+            if paddings is not None:
+                temp = tf.constant(value=1.0, shape=dim)
+                mask = tf.pad(temp, paddings=paddings)
+                y_true = mask * y_true
+                y_pred = mask * y_pred
+            psnr = tf.image.psnr(y_true, y_pred, max_val=1.)
+            return psnr
+
+        return masked_psnr
+
+def mse(dim, paddings=None, weight=1):
+        def masked_mse(y_true, y_pred):
+            if paddings is not None:
+                temp = tf.constant(value=1.0, shape=dim)
+                mask = tf.pad(temp, paddings=paddings)
+                y_true = mask * y_true
+                y_pred = mask * y_pred
+            return weight * mse
+
+        return masked_mse
+
+
+@tf.function
+def augment_prior(image:tf.Tensor, annotation:tf.Tensor):
+        MAX_ANGLE_PRIOR = tf.constant(0.25)
+        #MAX_ANGLE_PRIOR= 0.0873
+        NEEDLE_MAX_ANGLE = tf.constant(3.14)
+        image = tf.cast(image, dtype=tf.float32)
+        annotation = tf.cast(annotation, dtype=tf.float32)
+        needle_type = tf.random.uniform(minval=0, maxval=2, dtype=tf.int32, shape=[])
+        #needle_type = tf.constant(1)
+        needle = _get_needle_tensor(needle_type)
+        #needle = _get_needle_tensor(tf.constant(1, dtype=tf.int32))
+        # 0-none 1-rotate 2-scale 3-flip 4-flip_rotate
+        rand_num = tf.random.uniform(shape=[], minval=0, maxval=5, dtype=tf.int32, seed=10)
+        #rand_num = 4
+        angle = tf.random.uniform(shape=[], minval=-AUGMENTATION_MAX_ANGLE, maxval=AUGMENTATION_MAX_ANGLE,
+                                           dtype=tf.float32, seed=10)
+        needle_angle = tf.random.uniform(shape=[], minval=-NEEDLE_MAX_ANGLE, maxval=NEEDLE_MAX_ANGLE,
+                                  dtype=tf.float32, seed=15)
+        # dont rotate the prior image too much!
+        prior_angle = tf.random.uniform(shape=[], minval=-MAX_ANGLE_PRIOR, maxval=MAX_ANGLE_PRIOR,
+                                  dtype=tf.float32, seed=10)
+        scale_ratio = tf.random.uniform(shape=[], minval=0.8, maxval=1.0, dtype=tf.float32, seed=10)
+
+        if tf.equal(rand_num, tf.constant(1)):
+            image, annotation = _rotate(image, angle), _rotate(annotation, angle)
+        if tf.equal(rand_num, tf.constant(2)):
+            image, annotation = _scale(image, scale_ratio), _scale(annotation, scale_ratio)
+        if tf.equal(rand_num, tf.constant(3)):
+            image, annotation = _flip(image), _flip(annotation)
+        if tf.equal(rand_num, tf.constant(4)):
+            image, annotation = _flip_rotate(image, angle), _flip_rotate(annotation, angle)
+
+        if tf.equal(needle_type, tf.constant(0)):
+            needle = _rotate(needle, needle_angle)
+            image = tf.where(needle > 0., tf.random.uniform(shape=[], minval=0.9, maxval=1.5, dtype=tf.float32), image)
+        else:
+            needle = _translate(needle)
+            image = tf.where(needle > 0., tf.random.uniform(shape=[], minval=1.0, maxval=1.5, dtype=tf.float32),
+                                     image)
+
+        prior = _rotate(annotation, prior_angle)
+        annotation = tf.where(needle > 0., 1., annotation)
+        return [image, prior], annotation
+
+def _get_needle_numpy(needle_type):
+    #circle_flag = np.random.randint(0, 1)
+    needle_type
+    image_shape = (384, 384, 1)
+    x = image_shape[0]
+    y = image_shape[1]
+    max_x = x // 2
+    max_y = y // 2
+    start_x = np.random.randint(low=x // 3, high=max_x)
+    start_y = np.random.randint(low=y // 3, high=max_y)
+    needl = np.zeros(shape=image_shape)
+    if needle_type == 0: # rectangle
+        min_l = 10
+        max_l = np.maximum(x, y)
+        length = np.random.randint(low=min_l, high=max_l)
+        width = np.random.randint(low=1, high=3)
+        needl[start_x:start_x + width, start_y:start_y + length, :] = 1.0
+
+    if needle_type == 1: # circle
+        min_l = 2
+        max_l = 5
+        length = np.random.randint(low=min_l, high=max_l)
+        needl[start_x:start_x + length, start_y:start_y + length, :] = 1.0
+
+    needl[needl > 1] = 1.0
+    needl[needl < 1] = 0.0
+
+    return needl
+
+
+@tf.function(input_signature=[tf.TensorSpec(None, tf.int32)])
+def _get_needle_tensor(needle_type: tf.Tensor):
+    needle_tensor = tf.numpy_function(_get_needle_numpy, [needle_type], tf.double)
+    return needle_tensor
+
+@tf.function
+def insert_needle(image: tf.Tensor, annotation: tf.Tensor):
+        image = tf.cast(image, dtype=tf.float32)
+        annotation = tf.cast(annotation, dtype=tf.float32)
+        prior = tf.cast(annotation, dtype=tf.float32)
+        #needle = _get_needle_tensor(tf.random.uniform(minval=0, maxval=2, dtype=tf.int32))
+        needle = _get_needle_tensor(tf.constant(0, dtype=tf.int32))
+        image = tf.where(needle > 0., tf.random.uniform(shape=[], minval=0.9,maxval=1.5, dtype=tf.float32), image)
+        annotation = tf.where(needle > 0, 1.0, annotation)
+        return [image, prior], annotation
+
+
+@tf.function
+def _rotate(image: tf.Tensor, angle: tf.Tensor):
+        return tfa.image.rotate(image, angle, interpolation='BILINEAR')
+
+@tf.function
+def _translate(image: tf.Tensor):
+        x = tf.random.uniform(minval=-50, maxval=50, dtype=tf.int32, shape=[])
+        y = tf.random.uniform(minval=-50, maxval=50, dtype=tf.int32, shape=[])
+        return tfa.image.translate(image, [x,y], interpolation='nearest', fill_mode= 'constant', fill_value = 0.0)
+
+@tf.function
+def _flip(image: tf.Tensor):
+        return tf.image.flip_left_right(image)
+
+@tf.function
+def _flip_rotate(image: tf.Tensor, angle: tf.Tensor):
+        image = tf.image.flip_left_right(image)
+        return tfa.image.rotate(image, angle, interpolation='BILINEAR')
+
+@tf.function
+def _scale(image: tf.Tensor, ratio: tf.Tensor, dim=[384, 384, 1]):
+        print(ratio)
+        x1 = y1 = 0.5 - (0.5 * ratio)
+        x2 = y2 = 0.5 + (0.5 * ratio)
+        boxes = [x1,y1,x2,y2]
+        boxes = tf.cast(tf.reshape(boxes, shape=(1,4)), dtype=tf.float32)
+        scaled = tf.image.crop_and_resize([image], boxes=boxes, box_indices=tf.zeros(1, dtype=tf.int32),
+                                         crop_size=(dim[0], dim[1]))
+        scaled = tf.reshape(scaled, shape=(dim[0], dim[1], dim[2]))
+        #return tf.cast(scaled, dtype=tf.float16)
+        return scaled
+
+
+def lr_scheduler_linear(epoch, lr):
+    decay_rate = 0.5
+    decay_step = 5
+    print(epoch)
+    if (epoch > 0) and (epoch % decay_step == 0):
+        return lr * decay_rate
+    return lr
+
+def multiscale_ssim_l2(dim, weight=1, paddings=None, alpha=0.84):
+        def masked_mssim_l1(y_true, y_pred):
+            if paddings is not None:
+                temp = tf.constant(value=1.0, shape=dim)
+                mask = tf.pad(temp, paddings=paddings)
+                y_true = mask * y_true
+                y_pred = mask * y_pred
+            ms_ssim = 1 - tf.image.ssim_multiscale(y_true, y_pred, max_val=1.)
+            l1 = tf.reduce_mean(tf.keras.metrics.MAE(y_true, y_pred), axis=(-1, -2))
+            loss = (alpha * ms_ssim) + ((1 - alpha) * l1)
+            return weight * loss
+
+        return masked_mssim_l1
+
+
+def mssim(dim, weight=1., paddings=None):
+    def masked_mssim(y_true, y_pred):
+        if paddings is not None:
+            temp = tf.constant(value=1.0, shape=dim)
+            mask = tf.pad(temp, paddings=paddings)
+            y_true = mask * y_true
+            y_pred = mask * y_pred
+        ms_ssim = weight * tf.image.ssim_multiscale(y_true, y_pred, max_val=1.)
+        return ms_ssim
+    return masked_mssim
